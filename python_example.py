ae2b48865d722c24652c2b01adfbbf130320c864
litian <yang15465231@gmail.com>
2022-04-28 22:43:20 +0800

    haa

--- python_example.py
+++ python_example.py
@@ -17,7 +17,6 @@ app.secret_key = "super secret key"
 def index():
     return render_template("login.html")
 
-
 @app.route('/home')
 def home():
     return render_template('home.html', username = session['username'])
@@ -42,7 +41,6 @@ def login():
             return msg
     return render_template("login.html")
             
-        
 @app.route('/logout')
 def logout():
     session.pop('logged', None)
@@ -50,64 +48,117 @@ def logout():
     return redirect(url_for('login'))  
 
 
+#查可選課程
+@app.route('/can')
+def can():
+    results = """
+    <p><a href="/home">回到首頁</a></p>
+    """
+
+    can_add = "SELECT c_id,c_name FROM elective"
+    cursor = conn.cursor()
+    cursor.execute(can_add)
+    for i in cursor.fetchall():
+        results += "<p>{}   {}</p>".format(i[0],i[1])
+    return results
+
+
+
 #將必修直接加入已選課表
-@app.route('/compulsory' , methods=['POST'])
+@app.route('/compulsory')
 def compulsory():
-    # 建立資料庫連線
-    conn = MySQLdb.connect(host="127.0.0.1",
-                        user="hj",
-                        passwd="test1234",
-                        db="testdb")
+
+    results = """
+    <p><a href="/home">回到首頁</a></p>
+    """
 
     compulsory_count = 0
-    c = "SELECT c_id FROM compulsory where dept_grade = 2"
+    c = "SELECT c_id ,credit FROM compulsory where dept_grade = 2" #抓cid ,學分數
     cursor = conn.cursor()
     cursor.execute(c)
-    s = "SELECT s_id FROM student where 2022-register_year = 2"
+    s = "SELECT s_id FROM student where 2022-register_year = 2" #抓sid
     cursor1 = conn.cursor()
     cursor1.execute(s)
-    g = "SELECT credit FROM compulsory where dept_grade = 2"
-    cursor2 = conn.cursor()
-    cursor2.execute(s)
-    
-    for (i, ) in cursor.fetchall:
-        for (j, ) in cursor1.fetchall:
-            com ="INSERT INTO selection() VALUES ('{}','{}');".format(j,i)
+    list = []
+    for (i, ) in cursor1.fetchall():
+        list.append(i)
+    for i in cursor.fetchall():
+        for j in list:
+            already = "SELECT c_id FROM selection where s_id = '{}'".format(j) #抓出這個人選過的課，避免重複選取
+            cursor7 = conn.cursor()
+            cursor7.execute(already)
+            for (k, ) in cursor7.fetchall():
+                if k == i[0]:
+                    results += '已經選過了!!!'
+                    return results
+
+            com ="INSERT INTO selection() VALUES ('{}','{}');".format(j,i[0])
             cursor3 = conn.cursor()
             cursor3.execute(com)
             conn.commit() #插入選課列表中
-            compulsory_count += 1
+            compulsory_count += 1 #人數+1
 
-            c_count = "UPDATE compulsory SET count = '{}' where c_id = '{}'".format(compulsory_count,i) 
+            c_count = "UPDATE compulsory SET count = '{}' where c_id = '{}'".format(compulsory_count,i[0]) 
             cursor4 = conn.cursor()
             cursor4.execute(c_count)
-            conn.commit() #人數+1
+            conn.commit() #把人數存回去
 
             total_credit = "SELECT total_credit FROM student where s_id = '{}'".format(j)
             cursor5 = conn.cursor()
             cursor5.execute(total_credit)
             for k in  cursor5.fetchone():
                 total_credit = k
-                total_credit += g #計算總學分
+                total_credit += i[1] #計算總學分
             
             save_total_credit = "UPDATE student SET total_credit = '{}' where s_id = '{}'".format(total_credit,j) 
             cursor6 = conn.cursor()
             cursor6.execute(save_total_credit)
             conn.commit() #總學分加回去
-
         compulsory_count = 0
+    results += '已成功新增~'
+    return results
+
+@app.route('/FocusOn', methods=['GET', 'POST'])
+def FocusOn():
+    results = """
+    <p><a href="/home">回到首頁</a></p>
+    """
+
+    sid=request.form.get("sid1")
+    cid = request.form.get('focus')
+
+    insert = "INSERT INTO focuson() VALUES ('{}','{}');".format(sid,cid) #關注的課嫁進資料庫
+    cursor = conn.cursor()
+    cursor.execute(insert)
+    conn.commit()
+    results += '關注成功!'
+    return results
+
+# 列出已選課表
+@app.route('/SearchFocus', methods=['POST'])
+def SearchFocus():
+    # 取得輸入的文字
+    sid = request.form.get("sid2")
+
+    # 欲查詢的 query 指令
+    query_focuson = "SELECT c_id ,c_name ,count ,s_limit FROM elective where c_id in (SELECT c_id FROM focuson where s_id= '{}');".format(sid) #列出課表(必修)
+    # 執行查詢
+    cursor = conn.cursor()
+    cursor.execute(query_focuson)
+    
+    results = """
+    <p><a href="/home">回到首頁</a></p>
+    """
+    # 取得並列出所有查詢結果
+    for i,j,k,l in cursor.fetchall():
+        results += "<p>{} {} 已選人數為{}/{}</p>".format(i,j,k,l)
+    return results 
 
 # 加選
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     sid=request.form.get("add")
     cid = request.form.get('name1')
-
-    # 建立資料庫連線
-    conn = MySQLdb.connect(host="127.0.0.1",
-                        user="hj",
-                        passwd="test1234",
-                        db="testdb")
     
     results = """
     <p><a href="/home">回到首頁</a></p>
@@ -122,7 +173,6 @@ def add():
     if sel != 0:
         results += '這堂課選過了'
         return results
-
 
     #判斷超修
     c_credit = "SELECT credit FROM elective where c_id='{}'".format(cid)   #欲選學分數
@@ -157,8 +207,40 @@ def add():
         cur_count = i
     if max_count == cur_count:
         results += '已達人數上限'
+        
+        count = "SELECT rank_id FROM queued where s_id='{}' and c_id='{}'".format(sid, cid) #排隊人數
+        cursor = conn.cursor() 
+        cursor.execute(count)
+        counted = 0
+        for j in cursor.fetchall():
+            if count == j[0]:
+                for i in cursor.fetchone():
+                    counted += i
+                    break
+            counted += 1
+        # results += "<p>{}<p>".format(counted)
+        # return results 
+        if counted > 0:
+            results += '已加選排隊過'
+        else:
+            cur_line = "SELECT max(rank_id) FROM queued where c_id='{}'".format(cid) #抓取排隊順位
+            cursor = conn.cursor()
+            cursor.execute(cur_line)
+            curr_line = 0
+            for j in cursor.fetchall():
+                if cur_line == j[0]:
+                    for i in cursor.fetchone():
+                        curr_line += i #跑不進去
+                curr_line += 1
+            # results += "<p>{}<p>".format(curr_line)
+            # return results 
+            line = "INSERT INTO queued(s_id, c_id, rank_id) VALUES('{}', '{}', '{}')".format(sid, cid, curr_line) #插入排隊順位 
+            cursor = conn.cursor()
+            cursor.execute(line)
+            conn.commit()
+            results += '<p>已參加遞補排隊<p>'
         return results 
-    
+
     #課程名稱重複
     class_name_select = "SELECT c_name FROM elective where c_id = '{}'".format(cid) #查詢課程名稱
     # 執行查詢(目前課程名稱)
@@ -183,27 +265,24 @@ def add():
                 results += '選到重覆課程!!!'
                 return results
 
-    # #判斷是否衝堂
-    # already_select = "SELECT count(*) FROM selection where c_id = '{}'".format(cid)
-    # cursor = conn.cursor()
-    # cursor.execute(already_select)
-    # for i in cursor.fetchone():
-    #     already_select=i
+    #判斷是否衝堂
+    class_time = "SELECT slot FROM course_time where c_id = '{}'".format(cid) #抓要加選的課的時間
+    cursor1 = conn.cursor()
+    cursor1.execute(class_time)
 
-    # if(i)
-    # class_time = "SELECT slot FROM course_time where c_id = '{}'".format(cid)
-    # cursor1 = conn.cursor()
-    # cursor1.execute(class_time)
+    all_class_time = "SELECT slot FROM course_time where c_id in (SELECT c_id FROM selection) and c_id != '{}'".format(cid) #抓已經選過的課的時間(不包含自己)
+    cursor2 = conn.cursor()
+    cursor2.execute(all_class_time)
 
-    # all_class_time = "SELECT slot FROM course_time where c_id != '{}'".format(cid)
-    # cursor2 = conn.cursor()
-    # cursor2.execute(all_class_time)
+    list = []
+    for (i, ) in cursor2.fetchall():
+        list.append(i)
 
-    # for (i, ) in cursor1.fetchall():
-    #     for (j, ) in cursor2.fetchall():
-    #         if i == j :
-    #             results += '衝堂，加選失敗'
-    #             return results        
+    for (i, ) in cursor1.fetchall():
+        for j in list:
+            if i == j :
+                results += '衝堂，加選失敗'
+                return results
 
     #加選成功，人數及學分加一
     I_classId = "INSERT INTO selection() VALUES ('{}','{}');".format(sid, cid)    #插入加選代號
@@ -223,18 +302,12 @@ def add():
     results += '加選成功'
     return results
 
-
-
 # 列出已選課表
 @app.route('/action', methods=['POST'])
 def action():
     # 取得輸入的文字
     sid = request.form.get("sid")
-    # 建立資料庫連線
-    conn = MySQLdb.connect(host="127.0.0.1",
-                        user="hj",
-                        passwd="test1234",
-                        db="testdb")
+
     # 欲查詢的 query 指令
     query_compulsory = "SELECT c_name FROM compulsory where c_id in (SELECT c_id FROM selection where s_id= '{}');".format(sid) #列出課表(必修)
     # 執行查詢
@@ -255,10 +328,7 @@ def action():
         results += "<p>{}</p>".format(i)
     return results 
 
-
-
-
-    # 退選
+# 退選
 @app.route('/quit', methods=['GET', 'POST'])
 def quit():
     sid=request.form.get("del")
@@ -285,19 +355,18 @@ def quit():
     count = 0
     cursor = conn.cursor()
     cursor.execute(com_id)
-    for j in cursor.fetchall():
-        if cid == j[0]:
-            for i in cursor.fetchone():
-                co_id += i     #假如為必修，id則放入co_id
-                com_credit = "SELECT credit FROM compulsory WHERE c_id='{}'".format(cid)
-                cursor = conn.cursor()
-                cursor.execute(com_credit)
-                for i in cursor.fetchone():
-                    com_credit = i     #假如為必修，此課程學分放入com_credit
-                if com_credit > 0:
-                    c_credit = com_credit
-                count += 1
-                break
+    
+    for i in cursor.fetchone():
+        co_id += i     #假如為必修，id則放入co_id
+        com_credit = "SELECT credit FROM compulsory WHERE c_id='{}'".format(cid)
+        cursor = conn.cursor()
+        cursor.execute(com_credit)
+        for i in cursor.fetchone():
+            com_credit = i     #假如為必修，此課程學分放入com_credit
+        if com_credit > 0:
+            c_credit = com_credit
+        count += 1
+        break
                 
     if count == 0 :
         # 欲查詢的是否為選修指令
